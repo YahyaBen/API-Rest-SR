16e0a1dd6c5d32e3bb41dfa434a8f4338b79a76b
Yahya BENABDALLAH <Hyrkul@outlook.com>
2020-07-27 16:15:26 +0200

    Ajout API Controllers

---
+++ API_SR_Bank/API_SR_Bank/Controllers/ActionsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_SR_Bank.Models;
+
+namespace API_SR_Bank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActionsController : ControllerBase
+    {
+        private readonly MyContexte _context;
+
+        public ActionsController(MyContexte context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Actions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Action>>> GetActions()
+        {
+            return await _context.Actions.ToListAsync();
+        }
+
+        // GET: api/Actions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.Action>> GetAction(int id)
+        {
+            var action = await _context.Actions.FindAsync(id);
+
+            if (action == null)
+            {
+                return NotFound();
+            }
+
+            return action;
+        }
+
+        // PUT: api/Actions/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAction(int id, Models.Action action)
+        {
+            if (id != action.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(action).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Actions
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Models.Action>> PostAction(Models.Action action)
+        {
+            _context.Actions.Add(action);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAction", new { id = action.Id }, action);
+        }
+
+        // DELETE: api/Actions/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Models.Action>> DeleteAction(int id)
+        {
+            var action = await _context.Actions.FindAsync(id);
+            if (action == null)
+            {
+                return NotFound();
+            }
+
+            _context.Actions.Remove(action);
+            await _context.SaveChangesAsync();
+
+            return action;
+        }
+
+        private bool ActionExists(int id)
+        {
+            return _context.Actions.Any(e => e.Id == id);
+        }
+    }
+}
